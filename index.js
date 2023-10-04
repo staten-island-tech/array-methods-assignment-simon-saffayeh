776f6173a3f4accb003c25d7981efb9a06589a91
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-10-04 10:13:32 -0400

    sfsdf
    sdfsd

---
+++ index.js
@@ -0,0 +1,21 @@
+const lot = []
+
+const car1 = { brand: "Toyota", color: "blue", year: 2022, mileage: 15000, features: ["GPS navigation", "Bluetooth connectivity", "Backup camera"] };
+const car2 = { brand: "Honda", color: "red", year: 2021, mileage: 18000, features: ["Sunroof", "Lane departure warning", "Keyless entry"] };
+const car3 = { brand: "Chevrolet", color: "silver", year: 2019, mileage: 22000, features: ["Apple CarPlay", "Android Auto", "Heated seats"] };
+const car4 = { brand: "BMW", color: "black", year: 2020, mileage: 20000, features: ["Leather seats", "Adaptive cruise control", "Premium sound system"] };
+const car5 = { brand: "Audi", color: "white", year: 2023, mileage: 5000, features: ["Panoramic sunroof", "Navigation system", "Parking assist"] };
+
+
+lot.push(car1)
+lot.push(car2)
+lot.push(car3)
+lot.push(car4)
+lot.push(car5)
+
+lot.forEach(i => {
+    console.log(i.brand)
+    i.features.forEach(j => {
+        console.log(j)
+    });
+});
